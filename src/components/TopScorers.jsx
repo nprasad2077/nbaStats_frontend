9b5aaf56cfbed52e0c0819587d9d1041b749b181
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-21 12:51:14 -0500

    need to go create top players backend route

--- src/components/TopScorers.jsx
+++ src/components/TopScorers.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 // Components
 import DropDownSeason from "./DropDownSeason";
 
 const TopScorers = () => {
+    const [topScorers, setTopScorers] = useState([])
+
+    // const fetchTopScorers = async (season) => {
+    //     try {
+    //         const response = await fetch(`http://127.0.0.1:8000/api/playerdata/season/2023/`)
+    //     }
+    // }
+
+
+
+
   return (
     <div class="">
       <h1 class="text-3xl font-semibold antialiased text-center">
