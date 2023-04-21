1123e20db48e0a801b236c2694529ec0e7871cf8
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-21 05:44:07 -0500

    update reducers and actions

--- src/actions/playerDataActions.jsx
+++ src/actions/playerDataActions.jsx
@@ -1,15 +1,44 @@
-import * as playerDataActions from '../constants/playerDataConstants'
+import axios from "axios";
+import * as playerDataActions from "../constants/playerDataConstants";
 
 export const fetchPlayerDataStart = () => ({
-    type: playerDataActions.FETCH_PLAYER_DATA_START,
-})
+  type: playerDataActions.FETCH_PLAYER_DATA_START,
+});
 
 export const fetchPlayerDataSuccess = (playerData) => ({
-    type: playerDataActions.FETCH_PLAYER_DATA_SUCCESS,
-    payload: playerData,
-})
+  type: playerDataActions.FETCH_PLAYER_DATA_SUCCESS,
+  payload: playerData,
+});
 
 export const fetchPlayerDataFailure = (error) => ({
- type: playerDataActions.FETCH_PLAYER_DATA_FAILURE,
- payload: error,
-})
+  type: playerDataActions.FETCH_PLAYER_DATA_FAILURE,
+  payload: error,
+});
+
+export const fetchPlayerDataPage = async (page, allData = []) => {
+  try {
+    const response = await axios.get(
+      `http://127.0.0.1:8000/api/playerdata/?page=${page}`
+    );
+    const data = response.data.results;
+    if (data.length === 0) {
+      return allData;
+    } else {
+      return await fetchPlayerDataPage(page + 1, allData.concat(data));
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const fetchAllPlayerData = () => async (dispatch) => {
+  dispatch(fetchPlayerDataStart());
+
+  try {
+    const allData = await fetchPlayerDataPage(1);
+    dispatch(fetchPlayerDataSuccess(allData));
+  } catch (error) {
+    dispatch(fetchPlayerDataFailure(error));
+  }
+};
