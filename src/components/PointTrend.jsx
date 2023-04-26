12013adee1438439f764f1856efdd85cd0f346f5
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-26 13:36:59 -0500

    line chart display

--- src/components/PointTrend.jsx
+++ src/components/PointTrend.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
+import { Line } from "react-chartjs-2";
 
 const PointTrend = () => {
   const [points, setPoints] = useState([]);
@@ -9,11 +10,73 @@ const PointTrend = () => {
       .then((data) => setPoints(data));
   }, []);
 
-  console.log(points);
+  const chartDataset3pt = useMemo(() => {
+    const labels = points.map((item) => item.season);
+    const made = points.map((item) => item.avg_three_made);
+    const attempts = points.map((item) => item.avg_three_attempts);
+
+    return {
+      labels,
+      datasets: [
+        {
+          label: "3pt FG Made",
+          data: made,
+          borderColor: "rgba(75, 192, 192, 1)",
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+        },
+        {
+          label: "3pt FG Attempts",
+          data: attempts,
+          borderColor: "rgba(54, 162, 235, 1)",
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+        },
+      ],
+    };
+  }, [points]);
+
+  const chartDataset2pt = useMemo(() => {
+    const labels = points.map((item) => item.season);
+    const made = points.map((item) => item.avg_two_made);
+    const attempts = points.map((item) => item.avg_two_attempts);
+
+    return {
+      labels,
+      datasets: [
+        {
+          label: "2pt FG Made",
+          data: made,
+          borderColor: "rgba(255, 99, 132, 1)",
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+        },
+        {
+          label: "2pt FG Attempts",
+          data: attempts,
+          borderColor: "rgba(153, 102, 255, 1)",
+          backgroundColor: "rgba(153, 102, 255, 0.2)",
+        },
+      ],
+    };
+  }, [points]);
 
   return (
     <div>
-      <h1 class="text-3xl font-semibold antialiased text-center mb-4">Point Trends</h1>
+      <h1 className="text-3xl font-semibold antialiased text-center mb-4">
+        Point Trends
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+          <h2 className="text-xl font-semibold antialiased text-center mb-2">
+            3pt Field Goals
+          </h2>
+          <Line data={chartDataset3pt} />
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold antialiased text-center mb-2">
+            2pt Field Goals
+          </h2>
+          <Line data={chartDataset2pt} />
+        </div>
+      </div>
     </div>
   );
 };
