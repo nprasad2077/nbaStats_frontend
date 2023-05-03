ea519042c1f35342167ff47efc40d3d3cdb2adeb
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-05-02 20:24:03 -0500

    table dashboard view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,11 +21,11 @@ const Home = () => {
 
   return (
     <div className="text-slate-600 text-center">
-      <h1 className="text-3xl font-semibold antialiased my-4 text-center">
+      <h1 className="text-3xl font-semibold antialiased mt-4 mb-6 text-center">
         Home
       </h1>
-      <div className="overflow-x-auto" data-theme="corporate">
-        <table className="table w-full">
+      <div className="overflow-x-auto" data-theme="winter">
+        <table className="table w-full text-center">
           {/* head */}
           <thead>
             <tr>
@@ -46,46 +46,23 @@ const Home = () => {
             </tr>
             {/* row 2 */}
             <tr>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
+              <td>Playoffs Season Data Range</td>
+              <td>{dbInfo && dbInfo.playoffs_season_range.season__min - 1}-{dbInfo && dbInfo.playoffs_season_range.season__min} to {dbInfo && dbInfo.playoffs_season_range.season__max - 1}-{dbInfo && dbInfo.playoffs_season_range.season__max}</td>
             </tr>
             {/* row 3 */}
             <tr>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
+              <td>Regular Season Total Players</td>
+              <td>{dbInfo && dbInfo.total_players_regular}</td>
+            </tr>
+            {/* row 4 */}
+            <tr>
+              <td>Playoffs Season Total Players</td>
+              <td>{dbInfo && dbInfo.total_players_playoffs}</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div>
-        {dbInfo && (
-          <>
-            <h4>DB Stats</h4>
-            <div class="mt-4">
-              <h5>
-                Regular Season Data Range:{" "}
-                {dbInfo.regular_season_range.season__min - 1}-
-                {dbInfo.regular_season_range.season__min} to{" "}
-                {dbInfo.regular_season_range.season__max - 1}-
-                {dbInfo.regular_season_range.season__max} season
-              </h5>
-              <h4>
-                Regular Season Total Players {dbInfo.total_players_regular}
-              </h4>
-            </div>
-            <div class="mt-4">
-              <h5>
-                Playoffs Season Data Range:{" "}
-                {dbInfo.playoffs_season_range.season__min - 1}-
-                {dbInfo.playoffs_season_range.season__min} to{" "}
-                {dbInfo.playoffs_season_range.season__max - 1}-
-                {dbInfo.playoffs_season_range.season__max} season
-              </h5>
-              <h4>Playoffs Total Players {dbInfo.total_players_playoffs}</h4>
-            </div>
-          </>
-        )}
-      </div>
+      
     </div>
   );
 };
