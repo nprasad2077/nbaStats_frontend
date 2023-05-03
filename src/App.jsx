ac0c149d0b49658334713ac206cad0e51cc3f7f3
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-05-02 20:49:28 -0500

    navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -15,54 +15,91 @@ import TopScorersPlayoffs from "./components/TopScorersPlayoffs";
 import TopAssistsPlayoffs from "./components/TopAssistsPlayoffs";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div class="bg-gray-100 min-h-screen">
       <div className="navbar bg-base-300" data-theme="winter">
-        <Link to="/" className="btn btn-ghost normal-case text-lg">
-          NBA Data
-        </Link>
-        <Link to="/topscorers" className="btn btn-ghost normal-case text-lg">
-          Top Scorers
-        </Link>
-        <Link
-          to="/three_two_point_trends"
-          className="btn btn-ghost normal-case text-lg"
-        >
-          Shooting Trends
-        </Link>
-        <Link to="/top_assists" className="btn btn-ghost normal-case text-lg">
-          Top Assists
-        </Link>
-        <Link to="/top_rebounds" className="btn btn-ghost normal-case text-lg">
-          Top Rebounders
-        </Link>
-        <Link to="/ppg_histogram" className="btn btn-ghost normal-case text-lg">
-          Points Per Game Histogram
-        </Link>
-        <Link
-          to="/top_pts_scatter_plot"
-          className="btn btn-ghost normal-case text-lg"
-        >
-          Top Scorers vs. Win Shares
-        </Link>
-        <Link
-          to="/topscorers_playoffs"
-          className="btn btn-ghost normal-case text-lg"
-        >
-          Top Scorers {"(playoffs)"}
-        </Link>
-        <Link
-          to="/top_assists_playoffs"
-          className="btn btn-ghost normal-case text-lg"
-        >
-          Top Assists {"(playoffs)"}
-        </Link>
-        <Link
-          to="/top_pts_scatter_plot_2009_playoffs"
-          className="btn btn-ghost normal-case text-lg"
-        >
-          Top Scorers vs. Win Shares {"(playoffs)"}
-        </Link>
+        <div className="container mx-auto px-2 py-2 flex flex-wrap items-center justify-between">
+          <Link to="/" className="btn btn-ghost normal-case text-2xl">
+            NBA Data
+          </Link>
+          <button
+            className="lg:hidden btn btn-ghost normal-case text-lg"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            Menu
+          </button>
+          <div
+            className={`${
+              menuOpen ? "flex" : "hidden"
+            } lg:flex w-full lg:w-auto flex-wrap`}
+          >
+            {/* First row of navbar items */}
+            <div className="flex flex-wrap lg:justify-start w-full lg:w-auto">
+              {/* Add first row navbar items here */}
+              <Link
+                to="/topscorers"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Top Scorers
+              </Link>
+              <Link
+                to="/three_two_point_trends"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Shooting Trends
+              </Link>
+              <Link
+                to="/top_assists"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Top Assists
+              </Link>
+              <Link
+                to="/top_rebounds"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Top Rebounders
+              </Link>
+              <Link
+                to="/ppg_histogram"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Points Per Game Histogram
+              </Link>
+              <Link
+                to="/top_pts_scatter_plot"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Top Scorers vs. Win Shares
+              </Link>
+            </div>
+            {/* Second row of navbar items */}
+            <div className="flex flex-wrap justify-center w-full lg:w-auto mt-2 lg:mt-0">
+              {/* Add second row navbar items here */}
+             
+              <Link
+                to="/topscorers_playoffs"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Top Scorers {"(playoffs)"}
+              </Link>
+              <Link
+                to="/top_assists_playoffs"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Top Assists {"(playoffs)"}
+              </Link>
+              <Link
+                to="/top_pts_scatter_plot_2009_playoffs"
+                className="btn btn-ghost normal-case text-lg mt-2 lg:mt-0 lg:ml-4"
+              >
+                Top Scorers vs. Win Shares {"(playoffs)"}
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
       <Routes>
         <Route exact path="/" element={<Home />} />
