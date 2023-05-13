b181607b677cc582447bc94c11ee8fbabf983396
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-05-13 17:21:34 -0500

    season selector works!

--- src/components/ShotChart.jsx
+++ src/components/ShotChart.jsx
@@ -1,8 +1,85 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import Plot from "react-plotly.js";
 import axios from "axios";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+const seasons = [
+  // { season: "2022-2023" },
+  { season: "2021-2022" },
+  { season: "2020-2021" },
+  { season: "2019-2020" },
+  { season: "2018-2019" },
+  { season: "2017-2018" },
+  { season: "2016-2017" },
+  { season: "2015-2016" },
+  { season: "2014-2015" },
+  { season: "2013-2014" },
+  { season: "2012-2013" },
+  { season: "2011-2012" },
+  { season: "2010-2011" },
+  { season: "2009-2010" },
+];
+
+const DropDownSeason = ({ selectedSeason, setSelectedSeason }) => {
+  return (
+    <div className="w-72 mt-4 text-black">
+      <Listbox value={selectedSeason} onChange={setSelectedSeason}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-indigo-300 sm:text-sm">
+            <span className="block truncate">{selectedSeason.season}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {seasons.map((season, seasonIdx) => (
+                <Listbox.Option
+                  key={seasonIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-indigo-100 text-indigo-900" : "text-gray-900"
+                    }`
+                  }
+                  value={season}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {season.season}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-indigo-600">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+};
 
 export default function ShotChart() {
+  const [selectedSeason, setSelectedSeason] = useState(seasons[0]);
   const [shotData, setShotData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [season, setSeason] = useState(2020);
@@ -19,8 +96,9 @@ export default function ShotChart() {
   };
 
   useEffect(() => {
+    const tailEndSeason = selectedSeason.season.split("-")[1];
     fetchData(
-      `https://nba-stats-db.herokuapp.com/api/shot_chart_data/${player}/${season}/`
+      `https://nba-stats-db.herokuapp.com/api/shot_chart_data/${player}/${tailEndSeason}/`
     )
       .then((data) => {
         setShotData(data);
@@ -30,19 +108,24 @@ export default function ShotChart() {
         console.log(err);
         setLoading(false);
       });
-  }, []);
+  }, [selectedSeason]);
 
   console.log(shotData);
 
   if (loading) {
-    return <div className="flex justify-center items-center mt-6"><progress className="progress progress-accent w-56"></progress></div>
+    return (
+      <div className="flex justify-center items-center mt-6">
+        <progress className="progress progress-accent w-56"></progress>
+      </div>
+    );
   }
 
   const madeShots = shotData.filter((shot) => shot.result);
   const missedShots = shotData.filter((shot) => !shot.result);
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
+
       <Plot
         data={[
           {
@@ -65,7 +148,7 @@ export default function ShotChart() {
         layout={{
           xaxis: { range: [0, 500], showticklabels: false },
           yaxis: { range: [0, 472], showticklabels: false },
-          title: `${player} ${season} Season/Playoff Shot Chart`,
+          title: `${player} ${selectedSeason.season.split("-")[1]} Season/Playoff Shot Chart`,
           width: 500,
           height: 472,
           autosize: false,
@@ -87,6 +170,12 @@ export default function ShotChart() {
           ],
         }}
       />
+      <div className="mt-10">
+        <DropDownSeason
+          selectedSeason={selectedSeason}
+          setSelectedSeason={setSelectedSeason}
+        />
+      </div>
     </div>
   );
 }
